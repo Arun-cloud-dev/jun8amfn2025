c6357a1c6d6b8c0977ca0bd35dcd313af2d7a3ba
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-07-08 19:52:07 +0530

    day 1

---
+++ JavaScript/script.js
@@ -0,0 +1,135 @@
+// let var const 
+
+
+// let x = 10; 
+// let y = 16;
+// let name = "John raj + lkhasde l;ahksf 89061239048  Doe";
+// console.log(name);
+// let isActive = true;
+// console.log(typeof isActive);
+
+
+// let age = null;
+// console.log(typeof age); // object (this is a known JavaScript quirk)
+
+// let address;
+// address = "123 Main St, City, Country"
+// console.log(typeof address);
+
+
+// let user = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30,
+//     isActive: true
+// },
+// user2 = {
+//     firstName: "Jane",
+//     lastName: "Doe",
+//     age: 25,
+//     isActive: false
+//     };
+
+
+// console.log( typeof user.firstName); // object
+
+
+
+
+
+
+// var v = 20;
+// var z = 60;
+
+
+// const PI = 3.14;
+// const MAX = 100;
+
+
+
+
+
+
+
+
+
+// number, string, boolean, null, undefined, object, symbol, bigint
+
+
+
+
+// let ar = 10;
+
+
+
+// {
+//     let ar = 40;
+//     console.log(ar);
+// }
+
+
+
+// console.log(ar);
+
+
+
+
+
+
+
+// var a = 10;
+// {
+//     var a = 20;
+//     console.log(a);
+// }
+
+// console.log(a); 
+
+// let adderss;
+
+// adderss = "123 Main St, City, Country";
+
+// console.log(adderss);
+
+
+
+// const a = 10;
+// const b = 20;
+
+// const sum = a + b;
+
+
+// {
+//     const sum = a + b + 10;
+//     console.log(sum); // 40
+// }
+// let a = 10;
+// {
+//     const a = 20;
+//     console.log(a);
+    
+// }
+
+
+
+// console.log(a);
+
+// let b = "arun";
+
+// b = 80;
+
+
+
+// const c = "arun";
+
+// c = 90;
+
+
+
+const ff = 684565465464654564564654565665564n; // bigint
+
+
+console.log(ff+1n);
+
+
+
